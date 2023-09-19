c039362024b6ee7a1c0d177586d5fa7532dd718e
BigZaya <sonubiisaiah@gmail.com>
2023-09-19 13:09:40 -0700

    main.h

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - takes funtion as parameter and update to 98
+ *
+ * Return: Always 0
+ */
+void reset_to_98(int *n)
+{
+	int n = 98;
+	int *p = &n
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+README file task 0x05-pointers_arrays_strings
